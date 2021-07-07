a2a95092d79b4d79bd4853c09be271580ea37b93
Brendon <brendon3007gabriel@gmail.com>
2021-07-07 14:59:56 -0300

    Repositorios GIT

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,7 @@
-import { func } from 'prop-types';
 import React, { useState, FormEvent } from 'react';
 import api from '../../services/api';
-import Repository from '../Repository';
-
 import { Title, Repositories, Form } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 
 interface Repository {
   full_name: string;
@@ -11,44 +9,88 @@ interface Repository {
   owner: {
     login: string;
     avatar_url: string;
-  }
+  };
 }
-
 const Dashboard: React.FC = () => {
-    const [newRepo, setNewRepo] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
-    async function handleAddRepository(event: FormEvent<HTMLFormElement>
-      ):Promise<void>{
-      event.preventDefault();
-      const response = await api.get<Repository>( `repos/${newRepo}`);
-      const repository = response.data;
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
 
-      setRepositories([...repositories, repository]);
-    }
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
 
-     return (
-      <>
-        <Title>Explore repositórios no Github</Title>
-        <Form onSubmit={handleAddRepository}>
-          <input value={newRepo}
-           onChange={e => setNewRepo(e.target.value)}
-           placeholder="Digite o nome do repositório" />
-          <button type="submit">Pesquisar</button>
-        </Form>
-        <Repositories>
-          {repositories.map(repository => (
-            <>
-              <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
-              <div>
-                <strong>{repository.full_name}</strong>
-                <p>{repository.description}</p>
-              </div>
-            </>
-          ))}
-        </Repositories>
-      </>
-      );
-    };
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+  return (
+    <>
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        {/*repositories.map(repository => (
+          <a key ={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight />
+          </a>
+        ))*/}
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/82897681?v=4"
+            alt="Jean"
+          />
+          <div>
+            <strong>jeanrgl/s-react-app-exercise</strong>
+            <p>asassasasasasa</p>
+          </div>
+          <FiChevronRight />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/82897681?v=4"
+            alt="Jean"
+          />
+          <div>
+            <strong>jeanrgl/s-react-app-exercise</strong>
+            <p>asassasasasasa</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/82897681?v=4"
+            alt="Jean"
+          />
+          <div>
+            <strong>jeanrgl/s-react-app-exercise</strong>
+            <p>asassasasasasa</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+
+    </>
+  );
+};
 
 export default Dashboard;
